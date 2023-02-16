3f216ff246604c1177f1303b6573daf33888d285
Muhammad Fikri Haryanto <mfharyantooo@gmail.com>
2023-02-17 00:25:14 +0700

    feat: add connection to database

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"log"
 	"to-do-list/handler"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	dsn := "host=localhost user=postgres password=postgres dbname=todolist port=5432"
+	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Database connection refused")
+	}
+
+	log.Print("Database connection success")
+
 	router := gin.Default()
 	router.GET("", handler.GetHelloWorld)
 
