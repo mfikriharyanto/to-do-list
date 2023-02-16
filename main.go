0e71e4ac726c86066a710d4165c815d3ab802e43
Muhammad Fikri Haryanto <mfharyantooo@gmail.com>
2023-02-16 23:02:32 +0700

    feat: add root router

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"to-do-list/handler"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	router.GET("", handler.GetHelloWorld)
+
 	router.Run(":8080")
 }
