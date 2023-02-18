f1bef63da42fa98f53cc5c2a9b4f7fed1e505c16
Muhammad Fikri Haryanto <mfharyantooo@gmail.com>
2023-02-18 07:36:02 +0700

    fix: update type and binding settings

--- models/todo.go
+++ models/todo.go
@@ -1,17 +1,12 @@
 package models
 
 type ToDo struct {
-	ID     		string	`json:"id"`
+	ID     		uint	`json:"id"`
 	Title  		string	`json:"title"`
 	Completed 	bool	`json:"completed"`
 }
 
 type CreateToDo struct {
 	Title		string	`json:"title" binding:"required"`
-	Completed 	bool 	`json:"completed" binding:"required"`
-}
-
-type UpdateToDo struct {
-	Title		string	`json:"title"`
-	Completed	bool 	`json:"completed"`
+	Completed 	bool 	`json:"completed"`
 }
