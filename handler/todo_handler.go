24086d6fa9938003a11510bf6dd872e21fd2a788
Muhammad Fikri Haryanto <mfharyantooo@gmail.com>
2023-02-18 07:37:42 +0700

    feat: add handler for get and post

--- handler/todo_handler.go
+++ handler/todo_handler.go
@@ -2,10 +2,49 @@ package handler
 
 import (
 	"net/http"
+	"to-do-list/models"
+	"to-do-list/services"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetHelloWorld(context *gin.Context) {
-	context.IndentedJSON(http.StatusOK, gin.H{"message": "Hello World"})
+	todos, _ := services.GetAllTodo()
+	context.IndentedJSON(http.StatusOK, gin.H{"message": "Hello World", "data": todos})
+}
+
+func GetTodos(context *gin.Context) {
+	todos, _ := services.GetAllTodo()
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func GetTodo(context *gin.Context) {
+	id := context.Param("id")
+	todo, err := services.GetTodoById(id)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": err.Error()})
+		return
+	}
+
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
+func PostTodo(context *gin.Context) {
+	var createTodo models.CreateToDo
+
+	err := context.ShouldBindJSON(&createTodo)
+	if err != nil {
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	todo, err := services.CreateTodo(createTodo)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	context.IndentedJSON(http.StatusCreated, todo)
 }
